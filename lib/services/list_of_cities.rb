fcca26142ce5ec345019b202d7c4fc26c6b9b824
Tech-A12 <tech-a12@Tech-A12s-MacBook-Air.local>
2018-06-12 14:07:50 +0100

    Managed to access the API

---
+++ lib/services/list_of_cities.rb
@@ -0,0 +1,28 @@
+require 'httparty'
+require 'json'
+
+class ListOfCitiesService
+  include HTTParty
+
+  # doesn't like it when I use base_uri 'https://api.openweathermap.orgdata/2.5'  so use whole address for each method
+
+
+
+  def get_weather
+    JSON.parse(self.class.get("https://api.openweathermap.org/data/2.5/weather?id=524901&APPID=41a4b369873fcaf6d5e69dc2f5a22e38").body)
+  end
+
+  def get_city_weather(city_id)
+    JSON.parse(self.class.get("https://api.openweathermap.org/data/2.5/weather?id=#{city_id}&APPID=41a4b369873fcaf6d5e69dc2f5a22e38").body)
+  end
+
+  
+end
+
+city = ListOfCitiesService.new
+puts city.get_city_weather("2172797")
+
+# response = ListOfCitiesService.new
+# p response.get_city_weather("Berlin")
+
+#My API key   41a4b369873fcaf6d5e69dc2f5a22e38
